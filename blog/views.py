8bbce40673af39b3eff34ceb596586e74798143f
Kamil <kamil.dominko@gmail.com>
2024-03-10 20:13:56 +0100

    Dodanie stronicowania.

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,20 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
-    context = {'posts': posts}
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # Trzy posty na każdej stronie.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Jeżeli zmienna page nie jest liczbą całkowitą,
+        # wówczas pobierana jest pierwsza strona wyników.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Jeżelli zmienna page ma wartość większą niż numer ostatniej strony
+        # wyników, wtedy pobierana jest ostatnia strona wyników.
+        posts = paginator.page(paginator.num_pages)
+    context = {'posts': posts, 'page': page}
     return render(request, 'blog/post/list.html', context)
 
 
